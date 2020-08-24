ee910a97c88094d9e1f6728cf666a847fa977587
Shibo Lyu <laosb@lns.unihubs.com>
2020-08-24 13:15:42 +0800

    Initial Commit

---
+++ SalmonDayoffScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SalmonDayoffScanner
+//
+//  Created by Shibo Lyu on 2020/8/24.
+//  Copyright © 2020 Inkwire Technology (Hangzhou) Co., Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
