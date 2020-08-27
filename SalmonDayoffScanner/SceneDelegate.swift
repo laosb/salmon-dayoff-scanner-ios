b0e91770e992f76fb787b93f5d10d4407ec5e753
Shibo Lyu <laosb@lns.unihubs.com>
2020-08-27 19:02:42 +0800

    feat: support for mac catalyst.

--- SalmonDayoffScanner/SceneDelegate.swift
+++ SalmonDayoffScanner/SceneDelegate.swift
@@ -24,10 +24,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     // Use a UIHostingController as window root view controller.
     if let windowScene = scene as? UIWindowScene {
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UIHostingController(rootView: contentView)
-        self.window = window
-        window.makeKeyAndVisible()
+      let window = UIWindow(windowScene: windowScene)
+      window.rootViewController = UIHostingController(rootView: contentView)
+      self.window = window
+      window.makeKeyAndVisible()
+      
+      #if targetEnvironment(macCatalyst)
+      windowScene.titlebar?.toolbar?.isVisible = false
+      windowScene.titlebar?.titleVisibility = .hidden
+      #endif
     }
   }
 
